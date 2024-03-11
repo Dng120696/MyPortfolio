6600444929c2669eb1dc25d6921478bf018efd81
Christian Patrick Nebab <dongyang0216@gmail.com>
2024-03-11 16:43:14 +0800

    adding my new project in rails

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,5 +1,6 @@
 import slackApp from "../assets/slack_app.png";
 import BankApp from "../assets/bankAndBudgetApp.png";
+import JournalApp from "../assets/journal_app.png";
 
 function Project() {
   return (
@@ -126,6 +127,51 @@ function Project() {
               src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg"
               className="w-8 md:w-9"
             />
+          </p>
+        </div>
+      </div>
+      <div>
+        <h1 className="mb-6 text-4xl font-medium">3. Journal App</h1>
+        <p className="text-2xl tracking-[1px] leading-[1.6] dark:text-gray-300 text-gray-600  mb-6">
+          The Journal Application is a comprehensive task and category
+          management platform designed to streamline the organization of daily
+          tasks and projects. It allows users to create a personalized account,
+          within which they can craft, modify, and categorize tasks for enhanced
+          productivity and task tracking.
+        </p>
+        <div className="relative w-[30rem] sm:w[35rem] md:w-[40rem] lg:w-[45rem] h-[15rem] sm:h-[20rem] md:h[22rem] lg:h-[24.2rem] bank_img overflow-hidden">
+          {" "}
+          <img src={JournalApp} alt="" className=" w-[inherit]  rounded-md " />
+          <p className="link_bank_app  flex items-center gap-6 absolute top-[-50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-100 w-[inherit]  h-[inherit]  justify-center">
+            <a
+              href="https://github.com/Dng120696/Journal-App-Batch-32"
+              className="text-4xl"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa-brands fa-github dark:text-black "></i>
+            </a>
+            <button className="text-3xl border-gray-700  py-1 px-4 dark:bg-white dark:text-gray-900 rounded-md font-bold hover:dark:bg-gray-900 hover:dark:text-white hover:dark:border-white hover:bg-gray-900 hover:text-white hover:border-white">
+              <a
+                href="https://mysite-pldf.onrender.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Live
+              </a>
+            </button>
+          </p>
+        </div>
+        <div className="text-xl md:text-2xl mt-4 flex items-center">
+          <strong className="mr-6 text-gray-700 dark:text-gray-400">
+            Technologies:
+          </strong>
+          <p className="flex items-center gap-2 text-3xl md:text-4xl">
+            <i className="devicon-tailwindcss-original colored"></i>
+            <i className="devicon-css3-plain colored"></i>
+            <i className="devicon-ruby-plain colored"></i>
+            <i className="devicon-rails-plain colored"></i>
+            <i className="devicon-postgresql-plain colored"></i>
           </p>
         </div>
       </div>
